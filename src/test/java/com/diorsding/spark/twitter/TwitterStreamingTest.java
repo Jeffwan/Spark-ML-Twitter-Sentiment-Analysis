f2f93d8926c3d3e4a58e0f11742b73864aef0f0d
Jiaxin Shan <seedjeffwan@gmail.com>
2017-01-05 10:55:42 -0800

    Virtualization clean up

--- src/test/java/com/diorsding/spark/twitter/TwitterStreamingTest.java
+++ src/test/java/com/diorsding/spark/twitter/TwitterStreamingTest.java
@@ -1,7 +1,33 @@
 package com.diorsding.spark.twitter;
 
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
 /**
  * Created by jiashan on 12/29/16.
  */
 public class TwitterStreamingTest {
+
+    @Test
+    public void test() {
+        Jedis jedis = new Jedis(Constants.REDIS_CONNECTION_HOST, Constants.REDIS_CONNECTION_PORT);
+        Pipeline pipelined = jedis.pipelined();
+
+        for (int i =0 ; i < 100; i++) {
+            pipelined.publish("test", "houhouhou " + i);
+            System.out.println("houhouhou " + i);
+        }
+        pipelined.sync();
+    }
+
+    @Test
+    public void test2() {
+        Jedis jedis = new Jedis(Constants.REDIS_CONNECTION_HOST, Constants.REDIS_CONNECTION_PORT);
+
+        jedis.set("foo", "bar");
+
+        System.out.print(jedis.get("foo"));
+    }
 }
